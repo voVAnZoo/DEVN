5b3cfa5c7dcc16ab02865211bebcaff8adbbeefb
werty144 <anton-paramonov2000@yandex.ru>
2017-06-14 12:46:13 +0300

    Сделал нормальную генерацию старта и финиша, поместил игрока в центр старта

---
+++ app/src/main/java/com/example/user/devn/PauseActivity.java
@@ -0,0 +1,9 @@
+package com.example.user.devn;
+
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by user on 6/14/17.
+ */
+
+public class PauseActivity extends AppCompatActivity {
+}
