47f2f28c2b5e414c4833df717e15c564c046e354
mishakras <irinabudtova59@gmail.com>
2018-04-30 11:23:43 +0300

    поправка

--- app/src/main/java/com/example/user/devn/Point.java
+++ app/src/main/java/com/example/user/devn/Point.java
@@ -1,19 +1,20 @@
 package com.example.user.devn;
 
 public class Point {
-    int num;
-    int x;
-    int y;
+    double num;
+    double x;
+    double y;
 
-    public Point(int num, int x, int y){
+    public Point(double num, double x, double y){
         this.num=num;
         this.x=x;
         this.y=y;
     }
 
-     static public int distance2(Point p1,Point p2){
-        return (p1.x-p2.x)^2+(p1.y-p2.y)^2;
+    static public double distance2(Point p1,Point p2){
+        return (p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y);
     }
+
     public boolean inCurcle(Point p1,int x, int y,int rad){
         Point p2 = new Point(x,y,1);
         return  distance2(p1,p2) <= rad*rad;
---
+++ app/src/main/java/com/example/user/devn/Triangle.java
@@ -0,0 +1,47 @@
+package com.example.user.devn;
+
+public class Triangle {
+    Point p1;
+    Point p2;
+    Point p3;
+    Edge e1;
+    Edge e2;
+    Edge e3;
+    public Triangle(Point p1,Point p2, Point p3){
+        this.p1=p1;
+        this.p2=p2;
+        this.p3=p3;
+        this.e1=new Edge(p1,p2);
+        this.e1=new Edge(p2,p3);
+        this.e1=new Edge(p3,p1);
+    }
+
+    public Point getP1(Triangle t) {
+        return p1;
+    }
+
+    public Point getP2(Triangle t) {
+        return p2;
+    }
+
+    public Point getP3(Triangle t) {
+        return p3;
+    }
+
+    public Point centre(Triangle t){
+        double x =(( p1.x * p1.x + p1.y * p1.y) * (p2.y - p3.y) +
+                ( p2.x * p2.x + p2.y * p2.y) * (p3.y - p1.y) +
+                ( p3.x * p3.x + p3.y * p3.y) * (p1.y - p2.y));
+        double y =(( p1.x * p1.x + p1.y * p1.y) * (p3.x - p2.x) +
+                ( p2.x * p2.x + p2.y * p2.y) * (p1.x - p3.x) +
+                ( p3.x * p3.x + p3.y * p3.y) * (p2.x - p1.x));
+        double d =( p1.x * (p2.y - p3.y) +
+                p2.x * (p3.y - p1.y) +
+                p3.x * (p1.y - p2.y)) * 2;
+        return new Point(x/d,x/d,y/d);
+    }
+    public double radius(Triangle t){
+        double per = (e1.lengthEdge(e1)+e2.lengthEdge(e2)+e2.lengthEdge(e2))/2;
+        return e1.lengthEdge(e1)*e2.lengthEdge(e2)*e2.lengthEdge(e2)/4/Math.sqrt(per*(per-e1.lengthEdge(e1))*(per-e2.lengthEdge(e2))*(per-e3.lengthEdge(e3)));
+    }
+}
